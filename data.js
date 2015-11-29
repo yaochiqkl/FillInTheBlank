6e5c4d74fcd0d7689c0d26ffcf847c6226c51fd7
unknown <301100849@qq.com>
2015-11-29 15:00:51 +0800

    improved

--- data.js
+++ data.js
@@ -1,23 +1,26 @@
+//文章
+var passage ="It's the first question parents ask when their child is diagnosed with autism (自闭症). Will his future brothers or sisters have a higher risk of 1 _____ it, too? According to the largest study of siblings (兄弟姐妹) in families with autism, the answer is yes. Among 664 children who had at least one older sibling with the developmental disorder, the 2 _____ risk of autism was nearly 19%, 3 _____ higher than previous sibling-recurrence estimates that were anywhere from 3% to 10%. Kids with more than one older autistic sibling had an even higher risk of the disorder: 32%.The 4 _____ suggest that genes play a key role in autism risk. But they also hint that other environmental factors 5 _____ by siblings, like influences in the womb (子宫), may be important as well.On the 6 _____ of the findings, the researchers recommend that doctors closely 7 _____ younger siblings of autistic children to pick up any early signs of the disorder, 8 _____ an unusually large head or delayed language development and communication skills. Evidence suggests that early 9_____ and diagnosis of autism can help children take advantage of therapies that can treat some of its 10_____";
 //正确答案
-//var answers = new Array("common","results","consequently","basis","detection","monitor","symbols","average","dramatically","symptoms");
+var answers = new Array("developing","average","dramatically","results","shared","basis","monitor","including","detection","sysmptoms");
 //选项
-//var options = new Array("common","results","consequently","basis","detection","monitor","symbols","average","dramatically","symptoms","reason","distributed","including","developing","shared");
+var options = new Array("developing","average","dramatically","results","shared","basis","monitor","including","detection","sysmptoms","reason","distributed","retroduce","confused","shared");
+//答案与解析
 var analysis = [
 	{
-		"answer":"第一题答案",
-		"structure":"第一题解析."
+		"answer":"第一题答案 average 平均的",
+		"structure":"第一题解析. be average "
 	},
 	{
 		"answer":"第二题答案",
-		"structure":"第二题解析"
+		"structure":"第二题解析 do sth."
 	},
 	{
 		"answer":"第三题答案",
-		"structure":"第三题解析"
+		"structure":"第三题解析 be sth."
 	},
 	{
 		"answer":"第四题答案",
-		"structure":"第四题解析"
+		"structure":"第四题解析 hello"
 	},
 	{
 		"answer":"第五题答案",
--- main.js
+++ main.js
@@ -1,7 +1,163 @@
-window.onload = initPage;
-function initPage() {
+ 		$(function(){
+ 			initPage();
+		}); 
+	function initPage(){
+		//init Options randomly
+		initOptions();
+		//init triangle
+		initTriangle();
+		//init dragable function
+		bindResize(document.getElementById('main')); 4
+		//bind click events
+		$(".question,.question_number").click(clickEffect);
+		$("td").mouseover(mouseOver).mouseout(mouseOut).click(tableClick);
+		//Submit and Checkout
+		$(".submit").click(submitAnswers);
+		$(".popUpWin .inquery").click(closePopUpWin);
+	}
+	function initOptions(){
+		options.sort(function(a,b){ return Math.random()>0.5 ? -1 : 1;});
+		var $ol = $(".content .foot .answer td");
+		var i = 0;
+		$ol.each(function(){$(this).html(options[i++]);});
+	}
+	function initTriangle(){
+	   	$("#triangle").remove();
+		var triangle = "<span id = 'triangle'>▲</div>";
+		$(".number").append(triangle);
+		$("#triangle").css({
+			"position":"relative",
+			"left":"-124px"
+		});
+	}
+	function moveTriangle($n){
+		$("#triangle").remove();
+		var triangle = "<span id = 'triangle'>▲</div>";
+		$(".number").append(triangle);
+		var left = $n*27.6 - 124;
+		$("#triangle").css({
+			"position":"relative",
+			"left":left+"px"
+		});
+	}
+	function bindResize(el){ 
+	       var els = el.style, 
+	           y = 0; 
+	       $("#drag").mousedown(function(e){ 
+	           //按下元素后，计算当前鼠标与对象计算后的坐标 
+	           y = e.clientY - el.offsetHeight + 20; 
+	           $(document).bind("mousemove",move).bind("mouseup",up) ;
+	           e.preventDefault() ;
+	       }); 
+	       function move(e){ 
+	           els.height = e.clientY - y + 'px' ;
+	       } 
+	       function up(){ 
+	            $(document).unbind("mousemove", move).unbind("mouseup", up) ;
+	       } 
+	} 
+	function clickEffect(){
+	   	var $n = parseInt($(this).attr("id").slice(-1));
+	   	$(".question").removeClass("selected");
+	   	$(".question:eq("+$n+")").addClass("selected");
+	   	//跳转锚点
+	   	//location.hash="question"+$n;
+	    $(".content .main").animate({scrollTop:$("#quesion0")},800);
+	   	//三角
+	   	moveTriangle($n);
+	}
+	function mouseOver(){
+		$(this).addClass("mouseover");
+		var answer = $(this).text();
+		var $answer_box = $(".question.selected");
+		$answer_box.text(answer); 
+	}
+	function mouseOut(){
+		$(this).removeClass("mouseover");
+		$(".selected:not(.answered)").text("");
+	}
+	function tableClick(){
+		var $current = $(".selected");
+		if ($current.length>0) {
+			var $n = parseInt($current.attr("id").slice(-1));
+			$current.removeClass("mouseover").removeClass("selected").addClass("answered");
+			$(".question_number:eq("+$n+")").addClass("answered");
+			$(this).addClass("answered");
+			var answer = $(this).text();
+			var $answer_box = $(".question.selected");
+			$answer_box.text(answer); 
+		}
+	}
+	function submitAnswers(){
+		$(".popUpWin,.maskLayer").css({
+			display:"block"
+		});
+		checkAnswers();
+		showAnalysis();
+	}
+	function closePopUpWin(){
+		$(".popUpWin,.maskLayer").css({
+			display:"none"
+		});
+		return false;
+	}
+	function checkAnswers () {
+		var accuracy = 0;
+		var answer = null;
+		$(".question").removeClass("selected").removeClass("answered");
+		$(".question_number").removeClass("selected").removeClass("answered");
+		for(var i = 0; i < 10; i++){
+			answer = $("#question"+i).text();
+			if( answer == answers[i]){
+				accuracy++;
+				$(".popUpWin .main td:eq("+i+")").addClass("correct");
+				$(".content .foot .number span:eq("+i+")").addClass("correct");
+				$("#question"+i).addClass("correct");
+			} else {
+				$(".popUpWin .main td:eq("+i+")").addClass("incorrect");
+				$(".content .foot .number span:eq("+i+")").addClass("incorrect");
+				$("#question"+i).addClass("incorrect");
+				//插入正确答案
+				if ($("#question"+i).text() === '') {
+					$("#question"+i).html("<span style='color:black;text-decoration:none;display:inline-block;'>-</span>");
+					$("#question"+i).addClass("undo");
+				}
+				var correct_answer = "<span class = 'correct_answer'>"+answers[i]+"</span>";
+				$("#question"+i).after(correct_answer);
+			}
+		}
 
-}
-function showQuestion() {
-	
-}
+		accuracy = accuracy*10+"%";
+		$(".outcome").text(accuracy);
+	}
+	function showAnalysis(){
+		initTriangle();
+		var $text = $("<div id='analysis'><div class='title'>答案</div><div class='content result'></div><div class='title'>结构</div><div class='content structure'></div></div>");
+		$(".content .answer table").remove();
+		$(".content .answer").append($text);
+		$("#analysis .result").text(analysis[0].answer);
+		$("#analysis .structure").text(analysis[0].structure);
+		$(".question,.question_number").unbind("click",clickEffect).click(showAnalysisClick);
+		$(".submit").remove();
+		location.hash="question0";
+		$(".tip0").addClass("highlight");
+	}
+	function showAnalysisClick(){
+		removeHighlight();
+		//cancel the solution of getting index
+		var $n = parseInt($(this).attr("id").slice(-1));
+		moveTriangle($n);
+		showDetails($n);
+		addHighlight($n);
+		location.hash="question"+$n;
+	}
+	function showDetails($n){
+		$("#analysis .result").text(analysis[$n].answer);
+		$("#analysis .structure").text(analysis[$n].structure);
+	}
+	function addHighlight($n){
+		$(".tip"+$n).addClass("highlight");
+	}
+	function removeHighlight(){
+		$(".content .main .text span").removeClass("highlight");
+	}
